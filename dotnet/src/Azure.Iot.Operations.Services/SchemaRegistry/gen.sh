293a5095dc7cf72a7edb1ca9ae1f86c7f16ab945
Tim Taylor <timtay@microsoft.com>
2024-09-13 16:01:15 -0700

    Fix code gen pathing in state store + schema registry scripts (#41)

--- dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
@@ -1,3 +1,5 @@
-../../../../../tools/codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile SchemaRegistry-1.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
+rm -rf ./dtmi_ms_adr_SchemaRegistry__1
+mkdir ./dtmi_ms_adr_SchemaRegistry__1
+../../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile SchemaRegistry-1.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
 cp -f /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry/dtmi_ms_adr_SchemaRegistry__1/*.cs dtmi_ms_adr_SchemaRegistry__1 -v
 rm -rf /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry -v
--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,6 +1,6 @@
-../../../../../tools/codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile dss.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.StateStore.Gen
 rm -rf ./StateStoreGen
 mkdir ./StateStoreGen
+../../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile ../../../../eng/dtdl/statestore.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.StateStore.Gen
 cp -f /tmp/Azure.Iot.Operations.Services.StateStore.Gen/dtmi_ms_aio_mq_StateStore__1/*.cs ./StateStoreGen -v
 cp -f /tmp/Azure.Iot.Operations.Services.StateStore.Gen/PassthroughSerializer.cs ./StateStoreGen -v
 rm -rf /tmp/Azure.Iot.Operations.Services.StateStore.Gen -v
