7e4ba95d390a282a829ddbdecd369dcda10e942a
Ryan Winter <ryanwinter@outlook.com>
2024-10-08 09:31:50 -0700

    update deploy script to m2 (#105)
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- tools/deployment/deploy-aio.sh
+++ tools/deployment/deploy-aio.sh
@@ -22,65 +22,43 @@ cd $script_dir
 # add/upgrade the azure-iot-ops extension
 az extension add --upgrade --name azure-iot-ops
 
-# Install Jetstack helm repository
-helm repo add jetstack https://charts.jetstack.io --force-update
-helm repo update
-
+# If its a nightly build, we need to install all the dependencies
 if [ "$deploy_type" = "nightly" ]; then
+    # Install Jetstack helm repository
+    helm repo add jetstack https://charts.jetstack.io --force-update
+
     # install cert-manager
-    helm upgrade cert-manager jetstack/cert-manager --install --create-namespace --version v1.15 --set crds.enabled=true --set extraArgs={--enable-certificate-owner-ref=true} --wait
+    helm upgrade cert-manager jetstack/cert-manager --install --create-namespace -n cert-manager --version v1.16 --set crds.enabled=true --set extraArgs={--enable-certificate-owner-ref=true} --wait
+
+    # install trust-manager
+    helm upgrade trust-manager jetstack/trust-manager --install --create-namespace -n cert-manager --wait
 
     # install AIO Broker
-    helm uninstall broker --ignore-not-found
+    helm uninstall broker -n azure-iot-operations --ignore-not-found
     helm install broker --atomic --create-namespace -n azure-iot-operations --version 0.7.0-nightly oci://mqbuilds.azurecr.io/helm/aio-broker --values ./yaml/broker-values.yaml --wait
 fi
 
-# clean up any deployed Broker pieces
-kubectl delete configmap client-ca-trust-bundle -n azure-iot-operations --ignore-not-found
-kubectl delete BrokerAuthentication -n azure-iot-operations --all
-kubectl delete BrokerListener -n azure-iot-operations --all
-kubectl delete Broker -n azure-iot-operations --all
-
-# install trust-manager with azure-iot-operations as the trusted domain
-helm upgrade trust-manager jetstack/trust-manager --install --create-namespace -n azure-iot-operations --set app.trust.namespace=azure-iot-operations --wait
-
-# install cert issuers and trust bundle
-kubectl apply -f yaml/certificates.yaml
-
-# Wait for CA trust bundle to be generated (for external connections to the MQTT Broker) and then push to a local file
-kubectl wait --for=create --timeout=30s secret/aio-broker-external-ca -n azure-iot-operations
-kubectl get secret aio-broker-external-ca -n azure-iot-operations -o jsonpath='{.data.ca\.crt}' | base64 -d > $session_dir/broker-ca.crt
-
 # create CA for client connections. This will not be used directly by a service so many of the fields are not applicable
-echo "my-ca-password" > /tmp/password.txt
+echo "my-ca-password" > $session_dir/password.txt
 rm -rf ~/.step
 step ca init \
     --deployment-type=standalone \
     --name=my-ca \
-    --password-file=/tmp/password.txt \
+    --password-file=$session_dir/password.txt \
     --address=:0 \
     --dns=notapplicable \
     --provisioner=notapplicable
 
-# create client certificate
-step certificate create client $session_dir/client.crt $session_dir/client.key \
-    -f \
-    --not-after 8760h \
-    --no-password \
-    --insecure \
-    --ca ~/.step/certs/intermediate_ca.crt \
-    --ca-key ~/.step/secrets/intermediate_ca_key \
-    --ca-password-file=/tmp/password.txt
-
 # create client trust bundle used to validate x509 client connections to the broker
+kubectl delete configmap client-ca-trust-bundle -n azure-iot-operations --ignore-not-found
 kubectl create configmap client-ca-trust-bundle \
     -n azure-iot-operations \
     --from-literal=client_ca.pem="$(cat ~/.step/certs/intermediate_ca.crt ~/.step/certs/root_ca.crt)"
 
-# Create a SAT auth file for local testing
-kubectl create token default --namespace azure-iot-operations --duration=86400s --audience=aio-internal > $session_dir/token.txt
-
 # setup new Broker
 kubectl apply -f yaml/aio-$deploy_type.yaml
+
+# Update the credientials locally for connecting to MQTT Broker
+./update-credentials.sh
 
 echo Setup complete, session related files are in the '.session' directory
---
+++ tools/deployment/update-credentials.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit # fail if any command fails
+
+# setup some variables
+session_dir=$(dirname $(readlink -f $0))/../../.session
+mkdir -p $session_dir
+
+# Wait for CA trust bundle to be generated (for external connections to the MQTT Broker) and then push to a local file
+kubectl wait --for=create --timeout=30s secret/azure-iot-operations-aio-ca-certificate -n cert-manager
+kubectl get secret azure-iot-operations-aio-ca-certificate -n cert-manager -o jsonpath='{.data.ca\.crt}' | base64 -d > $session_dir/broker-ca.crt
+
+# create client certificate
+step certificate create client $session_dir/client.crt $session_dir/client.key \
+    -f \
+    --not-after 8760h \
+    --no-password \
+    --insecure \
+    --ca ~/.step/certs/intermediate_ca.crt \
+    --ca-key ~/.step/secrets/intermediate_ca_key \
+    --ca-password-file=$session_dir/password.txt
+
+# Create a SAT auth file for local testing
+kubectl create token default --namespace azure-iot-operations --duration=86400s --audience=aio-internal > $session_dir/token.txt
