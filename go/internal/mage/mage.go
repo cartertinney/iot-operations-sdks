949ec9cdc9432bc2622577c9dbc67fda4cc71f1d
Carl LeCompte <clecompt@microsoft.com>
2024-10-14 12:51:26 -0700

    [Go] Unify mage build commands. (#128)

---
+++ go/internal/mage/mage.go
@@ -0,0 +1,126 @@
+package mage
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"os"
+
+	"github.com/princjef/mageutil/bintool"
+	"github.com/princjef/mageutil/shellcmd"
+)
+
+//go:embed .golangci.yml
+var golangci string
+
+var (
+	golines = bintool.Must(bintool.NewGo(
+		"github.com/segmentio/golines",
+		"v0.12.2",
+	))
+	linter = bintool.Must(bintool.New(
+		"golangci-lint{{.BinExt}}",
+		"1.61.0",
+		"https://github.com/golangci/golangci-lint/releases/download/v{{.Version}}/golangci-lint-{{.Version}}-{{.GOOS}}-{{.GOARCH}}{{.ArchiveExt}}",
+	))
+	documenter = bintool.Must(bintool.New(
+		"gomarkdoc{{.BinExt}}",
+		"1.1.0",
+		"https://github.com/princjef/gomarkdoc/releases/download/v{{.Version}}/gomarkdoc_{{.Version}}_{{.GOOS}}_{{.GOARCH}}{{.ArchiveExt}}",
+	))
+)
+
+// Format formats the code.
+func Format() error {
+	if err := golines.Ensure(); err != nil {
+		return err
+	}
+
+	return golines.Command("-m 80 --no-reformat-tags -w .").Run()
+}
+
+// Lint lints the code.
+func Lint() error {
+	if err := linter.Ensure(); err != nil {
+		return err
+	}
+
+	done, err := tmpFile(".golangci.yml", golangci)
+	if err != nil {
+		return err
+	}
+	defer done()
+
+	return linter.Command(`run`).Run()
+}
+
+// Doc generates documents for the code.
+func Doc() error {
+	if err := documenter.Ensure(); err != nil {
+		return err
+	}
+
+	return documenter.Command(`./...`).Run()
+}
+
+// Test runs the unit tests.
+func Test() error {
+	return shellcmd.Command(`go test -race -cover -timeout 30s ./...`).Run()
+}
+
+// TestClean runs the unit tests with no test cache.
+func TestClean() error {
+	return shellcmd.RunAll(
+		`go clean -testcache`,
+		`go test -race -cover -timeout 12s ./...`,
+	)
+}
+
+// CI runs format, lint, doc, and test.
+func CI() error {
+	if err := Format(); err != nil {
+		return err
+	}
+
+	if err := Lint(); err != nil {
+		return err
+	}
+
+	if err := Doc(); err != nil {
+		return err
+	}
+
+	return Test()
+}
+
+// Verify that no thrashing occurred.
+func Verify() error {
+	// Check git status for any modified files.
+	modified, err := shellcmd.Command(`git ls-files -mz`).Output()
+	if err != nil {
+		return err
+	}
+	if len(modified) > 0 {
+		files := bytes.Split(modified, []byte{0})
+		return fmt.Errorf(
+			`found modified files - %s`,
+			bytes.Join(files[:len(files)-1], []byte(", ")),
+		)
+	}
+	return nil
+}
+
+// CIVerify runs CI and verifies no thrashing occurred.
+func CIVerify() error {
+	if err := CI(); err != nil {
+		return err
+	}
+	return Verify()
+}
+
+func tmpFile(name, contents string) (func(), error) {
+	if err := os.WriteFile(name, []byte(contents), 0o600); err != nil {
+		return nil, err
+	}
+	return func() { os.Remove(name) }, nil
+}
--- go/test/protocol/magefile.go
+++ go/test/protocol/magefile.go
@@ -3,90 +3,40 @@
 
 package main
 
+//mage:import
 import (
-	"bytes"
-	"fmt"
-
-	"github.com/princjef/mageutil/bintool"
+	"github.com/Azure/iot-operations-sdks/go/internal/mage"
 	"github.com/princjef/mageutil/shellcmd"
 )
 
-var (
-	golines = bintool.Must(bintool.NewGo(
-		"github.com/segmentio/golines",
-		"v0.12.2",
-	))
-	linter = bintool.Must(bintool.New(
-		"golangci-lint{{.BinExt}}",
-		"1.61.0",
-		"https://github.com/golangci/golangci-lint/releases/download/v{{.Version}}/golangci-lint-{{.Version}}-{{.GOOS}}-{{.GOARCH}}{{.ArchiveExt}}",
-	))
-)
-
-// Format formats the code.
-func Format() error {
-	if err := golines.Ensure(); err != nil {
-		return err
-	}
-
-	return golines.Command(`-m 80 --no-reformat-tags -w .`).Run()
-}
-
-// Lint lints the code.
-func Lint() error {
-	if err := linter.Ensure(); err != nil {
-		return err
-	}
-
-	return linter.Command(`run`).Run()
-}
-
 // Test runs the unit tests.
 func Test() error {
+	// Cannot use -race, since the wallclock tests register as racy.
 	return shellcmd.Command(`go test`).Run()
 }
 
 // TestClean runs the unit tests with no test cache.
 func TestClean() error {
+	// Cannot use -race, since the wallclock tests register as racy.
 	return shellcmd.RunAll(`go clean -testcache`, `go test`)
 }
 
 // CI runs format, lint, and test.
 func CI() error {
-	if err := Format(); err != nil {
+	if err := mage.Format(); err != nil {
 		return err
 	}
 
-	if err := Lint(); err != nil {
+	if err := mage.Lint(); err != nil {
 		return err
 	}
 
-	if err := Test(); err != nil {
-		return err
-	}
-
-	return nil
+	return Test()
 }
 
-// CIVerify runs CI and verifies no thrashing occurred.
 func CIVerify() error {
-	// Run CI.
 	if err := CI(); err != nil {
 		return err
 	}
-
-	// Check git status for any modified files.
-	modified, err := shellcmd.Command(`git ls-files -mz`).Output()
-	if err != nil {
-		return err
-	}
-	if len(modified) > 0 {
-		files := bytes.Split(modified, []byte{0})
-		return fmt.Errorf(
-			`found modified files - %s`,
-			bytes.Join(files[:len(files)-1], []byte(", ")),
-		)
-	}
-
-	return nil
+	return mage.Verify()
 }
