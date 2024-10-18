60d4baa80ab9a9190dfc86a9ac26dc1c2bd7b010
Carter Tinney <catinney@microsoft.com>
2024-10-18 10:59:27 -0700

    [Rust] Update release script (#155)
    Fixed pathing

--- rust/release.sh
+++ rust/release.sh
@@ -8,6 +8,6 @@ script_dir=$(dirname $(readlink -f $0))
 cd $script_dir
 
 echo -n Basic $(echo -n PAT:$PAT | base64) | cargo login --registry aio-sdks
-cargo publish --manifest-path ../azure_iot_operations_mqtt/Cargo.toml --registry aio-sdks
-cargo publish --manifest-path ../azure_iot_operations_protocol/Cargo.toml --registry aio-sdks
-cargo publish --manifest-path ../azure_iot_operations_services/Cargo.toml --registry aio-sdks
+cargo publish --manifest-path ./azure_iot_operations_mqtt/Cargo.toml --registry aio-sdks
+cargo publish --manifest-path ./azure_iot_operations_protocol/Cargo.toml --registry aio-sdks
+cargo publish --manifest-path ./azure_iot_operations_services/Cargo.toml --registry aio-sdks
