2249ad0ef7ce2a52e32f6fc2960471f58a336484
Tim Taylor <timtay@microsoft.com>
2025-02-13 11:17:00 -0800

    [ProtocolCompiler] Make codegen output target 0.8.0 .NET protocol compiler package (#469)

--- dotnet/samples/TestEnvoys/Math/FibResponseSchema.cs
+++ dotnet/samples/TestEnvoys/Math/FibResponseSchema.cs
@@ -39,6 +39,7 @@ namespace TestEnvoys.Math {
 
   }
   #region Messages
+  [global::System.Diagnostics.DebuggerDisplayAttribute("{ToString(),nq}")]
   public sealed partial class FibResponseSchema : pb::IMessage<FibResponseSchema>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
       , pb::IBufferMessage
@@ -249,7 +250,11 @@ namespace TestEnvoys.Math {
     #else
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
             break;
@@ -275,7 +280,11 @@ namespace TestEnvoys.Math {
     void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
             break;
--- dotnet/samples/TestEnvoys/Math/IsPrimeRequestPayload.cs
+++ dotnet/samples/TestEnvoys/Math/IsPrimeRequestPayload.cs
@@ -38,6 +38,7 @@ namespace TestEnvoys.Math {
 
   }
   #region Messages
+  [global::System.Diagnostics.DebuggerDisplayAttribute("{ToString(),nq}")]
   public sealed partial class IsPrimeRequestPayload : pb::IMessage<IsPrimeRequestPayload>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
       , pb::IBufferMessage
@@ -196,7 +197,11 @@ namespace TestEnvoys.Math {
     #else
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
             break;
@@ -218,7 +223,11 @@ namespace TestEnvoys.Math {
     void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
             break;
