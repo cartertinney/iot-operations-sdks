b97daccac4c69285ca30c9447b005e216cf46fea
Carl LeCompte <clecompt@microsoft.com>
2025-03-03 14:37:04 -0800

    [Go] Generate coverage profile files (#529)

--- go/test/protocol/magefile.go
+++ go/test/protocol/magefile.go
@@ -6,21 +6,29 @@
 
 package main
 
-import (
-	"github.com/Azure/iot-operations-sdks/go/internal/mage"
-	"github.com/princjef/mageutil/shellcmd"
-)
+import "github.com/Azure/iot-operations-sdks/go/internal/mage"
+
+var coverPkg = []string{
+	`github.com/Azure/iot-operations-sdks/go/internal`,
+	`github.com/Azure/iot-operations-sdks/go/mqtt`,
+	`github.com/Azure/iot-operations-sdks/go/protocol`,
+}
 
 // Test runs the unit tests.
 func Test() error {
 	// Cannot use -race, since the wallclock tests register as racy.
-	return shellcmd.Command(`go test`).Run()
+	return mage.Tester{
+		CoverPkg: coverPkg,
+	}.Run()
 }
 
 // TestClean runs the unit tests with no test cache.
 func TestClean() error {
 	// Cannot use -race, since the wallclock tests register as racy.
-	return shellcmd.RunAll(`go clean -testcache`, `go test`)
+	return mage.Tester{
+		Clean:    true,
+		CoverPkg: coverPkg,
+	}.Run()
 }
 
 // CI runs format, lint, and test.
