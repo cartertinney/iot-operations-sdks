558f649a492fb95a14d92e4d59c137aaa8b5ded5
Tim Taylor <timtay@microsoft.com>
2025-03-12 11:45:04 -0700

    [.NET] Fix bug where MQTT client closed connection upon receiving MQTT auth packet in response to re-auth (#586)
    MQTTnet's client implementation will close the connection if it receives
    a MQTT auth packet but no handler is set. This adds a handler by default
    that simply logs the received response status code
    
    
    https://github.com/dotnet/MQTTnet/blob/93a9d8d1dbe8dde2b7d79a37c873fae13b4f4797/Source/MQTTnet/MqttClient.cs#L660
    
    Note that this bug broke token renewal for the .NET SDK, but it now
    should work as expected

---
+++ dotnet/src/Azure.Iot.Operations.Mqtt/SatEnhancedAuthenticationHandler.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using MQTTnet;
+
+namespace Azure.Iot.Operations.Mqtt
+{
+    /// <summary>
+    /// This defines how an MQTT client should respond upon receiving an MQTT auth packet when using SAT authentication.
+    /// </summary>
+    /// <remarks>
+    /// The typical flow for SAT auth is that the client proactively sends an MQTT auth packet to the broker with updated
+    /// credentials, and then the broker sends back an MQTT auth packet that simply details if the client's auth packet was
+    /// accepted or not. The client doesn't need to respond with another auth packet, so this handler simply logs the status code
+    /// in the received MQTT auth packet.
+    /// </remarks>
+    internal class SatEnhancedAuthenticationHandler : IMqttEnhancedAuthenticationHandler
+    {
+
+        public Task HandleEnhancedAuthenticationAsync(MqttEnhancedAuthenticationEventArgs eventArgs)
+        {
+            if (eventArgs.ReasonCode == MQTTnet.Protocol.MqttAuthenticateReasonCode.Success)
+            {
+                Trace.TraceInformation("Received re-authentication response from MQTT broker with status {0}", eventArgs.ReasonCode);
+            }
+            else
+            {
+                Trace.TraceError("Received re-authentication response from MQTT broker with status {0} and reason string {1}", eventArgs.ReasonCode, eventArgs.ReasonString);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
