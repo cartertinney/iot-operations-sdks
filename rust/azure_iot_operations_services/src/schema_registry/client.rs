a0f98536f56212d19b6faa0a8525298c976c1fd6
Raul Leclair <raulleclair@microsoft.com>
2025-03-17 15:54:10 -0700

    [Rust][Services] Temporary mitigation for Schema Registry 404 error (#604)
    # Context
    The schema registry service is currently returning an empty payload when
    a Get operation is performed on a non-existent schema. This results in a
    PayloadInvalid error because an empty payload is not valid JSON.
    
    # Changes
    A temporary mitigation has been added to the Schema Registry client's
    get operation to address the 404 case. This mitigation involves
    returning None when a PayloadInvalid error occurs due to a Get operation
    on a non-existent schema.

--- rust/azure_iot_operations_services/src/schema_registry/client.rs
+++ rust/azure_iot_operations_services/src/schema_registry/client.rs
@@ -165,13 +165,23 @@ where
                 SchemaRegistryError(SchemaRegistryErrorKind::InvalidArgument(e.to_string()))
             })?;
 
-        Ok(self
-            .get_command_invoker
-            .invoke(command_request)
-            .await
-            .map_err(SchemaRegistryErrorKind::from)?
-            .payload
-            .schema)
+        let get_result = self.get_command_invoker.invoke(command_request).await;
+
+        match get_result {
+            Ok(response) => Ok(response.payload.schema),
+            Err(e) => {
+                if let azure_iot_operations_protocol::common::aio_protocol_error::AIOProtocolErrorKind::PayloadInvalid = e.kind {
+                    if let Some(nested_error) = &e.nested_error {
+                        if let Some(json_error) = nested_error.downcast_ref::<serde_json::Error>() {
+                            if json_error.is_eof() && json_error.column() == 0 && json_error.line() == 1 {
+                                return Ok(None);
+                            }
+                        }
+                    }
+                }
+                Err(SchemaRegistryError(SchemaRegistryErrorKind::from(e)))
+            }
+        }
     }
 
     /// Adds or updates a schema in the schema registry service.
