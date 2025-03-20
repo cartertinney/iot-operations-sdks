0e7f10aedb236601a98833464cb3c3bc1eae8b6b
Carter Tinney <catinney@microsoft.com>
2025-03-20 08:45:35 -0700

    [Rust][MQTT]  Fixed hostname in manual_ack sample #2 (#628)
    Correction to #627

--- rust/azure_iot_operations_mqtt/examples/manual_ack.rs
+++ rust/azure_iot_operations_mqtt/examples/manual_ack.rs
@@ -14,7 +14,7 @@ use azure_iot_operations_mqtt::session::{
 use azure_iot_operations_mqtt::MqttConnectionSettingsBuilder;
 
 const CLIENT_ID: &str = "aio_example_client";
-const HOSTNAME: &str = "hostname";
+const HOSTNAME: &str = "localhost";
 const PORT: u16 = 1883;
 const TOPIC: &str = "hello/mqtt";
 
