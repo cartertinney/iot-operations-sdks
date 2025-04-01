d9b79b55331691882cf36eff460e06741d1ca9a2
Ryan Winter <ryanwinter@outlook.com>
2025-04-01 14:56:04 -0700

    E2E not failing if a step failed (#670)
    * Switching to use `if: always()` to force workflow to fail if a step
    fails
    * Fixed broken .net client/server due to path change
    * Update ClientServer to return a connected mqttclient from the factory,
    because it needs to be connected before the CounterServer is
    constructed.
    
    ---------
    
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- dotnet/samples/Protocol/Counter/CounterServer/RpcHostBackgroundService.cs
+++ dotnet/samples/Protocol/Counter/CounterServer/RpcHostBackgroundService.cs
@@ -7,15 +7,12 @@ using Azure.Iot.Operations.Mqtt.Session;
 
 namespace CounterServer;
 
-public class RpcHostBackgroundService(MqttSessionClient mqttClient, CounterService counterService, ILogger<RpcHostBackgroundService> logger) : BackgroundService
+public class RpcHostBackgroundService(CounterService counterService) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        MqttConnectionSettings mcs = MqttConnectionSettings.FromEnvVars();
-
-        MqttClientConnectResult connAck = await mqttClient.ConnectAsync(mcs, stoppingToken);
-        logger.LogInformation("Connected to: {mcs} with session present: {s}", mcs, connAck.IsSessionPresent);
         await counterService.StartAsync(null, cancellationToken: stoppingToken);
     }
+
     protected ValueTask DisposeAsync() => counterService!.DisposeAsync();
 }
